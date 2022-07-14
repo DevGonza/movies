0c7ebe8334b5cc8a79f1b76d2976e73450caf1b5
DevGonza <gonzalo.sanchez.s@gmail.com>
2022-07-14 12:48:54 -0300

    page error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './css/App.css';
 import Favoritos from './Pages/Favoritos/Favoritos';
 import PeliculasDestacadas from './Pages/PeliculasDestacadas/PeliculasDestacadas';
 import Peliculas from './Pages/Peliculas/Peliculas';
+import Error from './Pages/Error/Error';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/detalle/:id' element= {<DetallePelicula />} />
         <Route path='/favoritos' element= {<Favoritos />} />
         <Route path='/peliculas' element={<Peliculas />} />
+        <Route path='*' element={<Error />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import error from '../../Assets/Img/error.jpg'
+
+const Error = () => {
+  return (
+    <div className='text-white'>
+        <img className='w-100' src={error}/>
+    </div>
+  )
+}
+
+export default Error
